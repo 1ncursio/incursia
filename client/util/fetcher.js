c7511317bf9055527a1f341ac4f5eea6682a8650
1ncursio <ckswn1323@naver.com>
2021-02-19 23:10:13 +0900

    apply .gitignore

--- client/util/fetcher.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://localhost:3100';
-axios.defaults.withCredentials = true;
-
-const fetcher = (url) => axios.get(url).then((result) => result.data);
-
-export default fetcher;
