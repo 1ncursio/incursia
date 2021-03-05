48cb3572a9f3862a95bf372cbea98b2d784dcf1e
1ncursio <ckswn1323@naver.com>
2021-03-05 20:43:35 +0900

    [UPDATE] migrate js to ts

--- client/typings/db.ts
+++ client/typings/db.ts
@@ -2,8 +2,13 @@ export interface IUser {
   id: number;
   nickname: string;
   email: string;
+  status: string;
+  profile: string;
 }
 
 export interface IPost {
   id: number;
+  title: string;
+  caption: string;
+  createdAt: string;
 }
