90a446a4388fdd200254ffa8b8ac17f7e4213251
1ncursio <ckswn1323@naver.com>
2021-04-02 16:30:45 +0900

    [UPDATE] cookie sharing

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 app.use(
   cors({
-    origin: ['http://localhost:3080', 'incursia.net', 'http://3.15.224.155'],
+    origin: ['http://localhost:3080', 'http://incursia.net'],
     credentials: true,
   })
 );
@@ -52,6 +52,11 @@ app.use(
     saveUninitialized: false,
     resave: false,
     secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      domain: process.env.NODE_ENV === 'production' && '.incursia.site',
+    },
   })
 );
 app.use(passport.initialize());
