99a9136cf340bd54f25c0f0cf0e57f062b09a81d
1ncursio <ckswn1323@naver.com>
2021-04-02 12:26:15 +0900

    [UPDATE] cors

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 app.use(
   cors({
-    origin: ['http://localhost:3080', 'incursia.net'],
+    origin: ['http://localhost:3080', 'incursia.net', 'http://3.15.224.155'],
     credentials: true,
   })
 );
